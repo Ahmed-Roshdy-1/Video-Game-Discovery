e17fc64ee467f2435865c1683d4dcef733111cbe
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2026-09-03 18:08:58 +0300

    add SortSelector component and integrate it into the main layout with PlatformSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, HStack } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,6 +6,7 @@ import PlatformSelector from "./components/PlatformSelector";
 import { useState } from "react";
 import type { Genre } from "./hooks/useGenres";
 import type { Platform } from "./hooks/usePlatforms";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -35,7 +36,10 @@ function App() {
         </GridItem>
       
       <GridItem area="main">
-        <PlatformSelector onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} selectedPlatform={gameQuery.platform} />
+        <HStack spacing={5} paddingLeft={10} paddingBottom={5}>
+          <PlatformSelector onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} selectedPlatform={gameQuery.platform} />
+          <SortSelector />
+        </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
 
