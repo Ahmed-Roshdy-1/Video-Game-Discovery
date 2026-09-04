b916afaff53a43a9d99b243bf0fe8fb7c81f41da
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2026-09-04 16:12:22 +0300

    add custom gray color palette to theme configuration

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,20 @@ const config : ThemeConfig = {
 
 const theme = extendTheme({
     config,
+   colors: {
+    gray: {
+        50: "#f9f9f9",
+        100: "#e0e0e0",
+        200: "#c0c0c0",
+        300: "#a0a0a0",
+        400: "#808080",
+        500: "#606060",
+        600: "#404040",
+        700: "#202020",
+        800: "#101010",
+        900: "#000000",
+    }
+   }
 })
 
 export default theme
