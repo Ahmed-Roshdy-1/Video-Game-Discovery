9688a7bd60e4387fc105a0959a91c95629796995
Ahmed-Roshdy-1 <ahmedroshdy01029@gmail.com>
2026-09-04 16:02:54 +0300

    add Emoji component to display game ratings in GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import type { Game } from "@/hooks/useGame";
 import { Card, CardBody, Image, Heading, Badge, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import getCroppedImageUrl from "@/services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
     game: Game;
@@ -13,12 +14,11 @@ const GameCard = ({ game }: Props) => {
         <Card>
             <Image src={getCroppedImageUrl(game.background_image)} alt={game.name}  width='100%' />
             <CardBody>
-                {/* truncate the game name if it is too long */}
-                <Heading fontSize='2xl' marginBottom={3} whiteSpace='nowrap' overflow='hidden' textOverflow='ellipsis'>{game.name}</Heading>
-                <HStack justifyContent='space-between'>
+                <HStack justifyContent='space-between' marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
                     <Badge fontSize='14px' colorScheme={badgeColor} paddingX={2} borderRadius='4px'>{game.metacritic}</Badge>
                 </HStack>
+                <Heading fontSize='2xl' whiteSpace='nowrap' overflow='hidden' textOverflow='ellipsis'>{game.name}<Emoji rating={game.rating_top} /></Heading>
             </CardBody>
         </Card>
     )
